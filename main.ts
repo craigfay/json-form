5c7843d1fa9f06ea7208b490efe779918041958b
Craig Fay <craigfay1@gmail.com>
2019-08-14 18:04:26 -0500

    Move form into html file

--- main.ts
+++ main.ts
@@ -6,101 +6,9 @@ const read = promisify(readFile);
 const write = promisify(writeFile);
 
 const keyFile = './.key';
-const settingsFile = './.settings.json'
+const settingsFile = './.settings.json';
+const settingsForm = './index.html';
 
-function editForm(json) {
-  return `
-    <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono&display=swap" rel="stylesheet">
-    <style>
-      * {
-        font-family: 'Roboto', sans-serif;
-        color: #777;
-        font-size: 14px;
-      }
-      form {
-        width: 40em;
-      }
-      form * {
-        display: block;
-        margin-bottom: 1em;
-      }
-      textarea {
-        font-family: 'Roboto Mono', monospace;
-        white-space: nowrap;
-        height: 80vh;
-        width: 100%;
-      }
-      button {
-        padding: .5rem 1rem;
-        width: 6rem;
-        color: #777;
-        border: .04rem solid #aaa;
-        border-radius: .4rem;
-      }
-      #key-errors:not(:empty),
-      #json-errors:not(:empty) {
-        padding: 1rem;
-        color: salmon;
-        background-color: lavenderblush;
-        border-radius: .4rem;
-      }
-
-    </style>
-    <form onsubmit="handleSubmit(event)" method="POST">
-      <input name="key" id="key" placeholder="key"/>
-      <p id="key-errors"></p>
-      <textarea name="json" id="json"></textarea>
-      <p id="json-errors"></p>
-      <button>SAVE</button>
-    </form>
-    <script>
-      const keyField = document.querySelector('#key');
-      const keyErrors = document.querySelector('#key-errors');
-      const jsonField = document.querySelector('#json');
-      const jsonErrors = document.querySelector('#json-errors');
-
-      async function fetchSettings() {
-        const response = await fetch('/settings.json');
-        jsonField.value = await response.text();
-      }
-
-      fetchSettings();
-
-      keyField.addEventListener('keyup', validateKey);
-      jsonField.addEventListener('keyup', validateJson);
-
-      function validateKey() {
-        if (keyField.value != '') {
-          keyErrors.innerText = '';
-          return true;
-        } else {
-          keyErrors.innerText = 'Key must be provided';
-          return false;
-        }
-      }
-      function validateJson() {
-        try {
-          JSON.parse(jsonField.value);
-          jsonErrors.innerText = '';
-          return true;
-        } catch (error) {
-          jsonErrors.innerText = error.message;
-          return false;
-        }
-      }
-      function handleSubmit(event) {
-        if (validateKey() && validateJson()) return;
-        event.preventDefault();
-      }
-    </script>
-    `
-}
-
-// Respond with a form to edit the settings.json
-async function getSettings(req?) {
-  const json = await read(settingsFile, 'utf8');
-  return editForm(json);
-}
 
 // Handle form submissions seeking to edit settings.json
 async function saveSettings(req?) {
@@ -112,12 +20,12 @@ async function saveSettings(req?) {
   return 401; // Unauthorized
 
   await write(settingsFile, json)
-  return getSettings();
+  return read(settingsForm, 'utf8');
 }
 
 const s = http.server();
 s.route('GET', '/settings.json', () => read(settingsFile, 'utf8'));
-s.route('GET', '/*', getSettings);
+s.route('GET', '/*', () => read(settingsForm, 'utf8'));
 s.route('POST', '/*', saveSettings);
 s.listen(5000);
 console.log('listening ...');
